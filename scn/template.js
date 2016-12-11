361bceb1c2c43d2820e82108ce6f9e71935608f4
Les K. Johnson <les@leskjohnson.com>
2016-12-11 00:30:42 -0500

    Add a scene template file

---
+++ scn/template.js
@@ -0,0 +1,10 @@
+var template_scene={
+	'setup':function(){},
+	'init':function(){},
+	'start':function(){},
+	'stop':function(){},
+	'draw':function(){},
+	'html':function(){},
+	'sett':function(){},
+	'events':function(){},
+};
