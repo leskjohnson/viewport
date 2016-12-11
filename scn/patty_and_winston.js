4be2d21991663a80a461b26f0272580cb370d2e9
Les K. Johnson <les@leskjohnson.com>
2016-12-11 00:12:18 -0500

    Update Patty and Winston sprites

--- scn/patty_and_winston.js
+++ scn/patty_and_winston.js
@@ -1,6 +1,7 @@
 /* ========================================================= Patty and Winston Object ====== */
 /* patty and winston object ---------------------------------------------------------------- */
 var patty_and_winston={
+	'active_char':0,
 	/* the array for holding all the characters */
 	'characters':[],
 	/* the force that pushes the characters */
@@ -8,23 +9,25 @@ var patty_and_winston={
 	/* the dimensions of the container */
 	'dimensions':{'width':500,'height':200},
 	/* settings called before viewport setup */
+	'background':new Image(),
 	'setup':function(){
 		settings.view='patty_and_winston';
-		settings.dimensions.width=patty_and_winston.dimensions.width;
-		settings.dimensions.height=patty_and_winston.dimensions.height ;
+		settings.dimensions.width=this.dimensions.width;
+		settings.dimensions.height=this.dimensions.height ;
+		this.background.src='img/grass.jpg';
 	},
 	/* after viewport is setup */
 	'init':function(){},
 	/* start the patty_and_winston experience */
 	'start':function(){
-		this.events(true);
 		this.characters_create();
+		this.events(true);
 		window.requestNextAnimationFrame(animate);
 	},
 	/* stop the patty_and_winsont experience */
 	'stop':function(){
-		this.events(false);
 		this.characters_destroy();
+		this.events(false);
 	},
 	'reset':function(){
 		this.stop();
@@ -33,18 +36,32 @@ var patty_and_winston={
 	/* add characters to the characters array */
 	'characters_create':function(){
 		var ch=[];
-		ch[ch.length]=new character(
-			'patty',/* the name of the character */
-			-128,/* x position */
-			50,/* y position */
-			1/* speed */
-		);
-		ch[ch.length]=new character(
-			'winston',/* the name of the character */
-			-128,/* x position */
-			100,/* y position */
-			0.75/* speed */
-		);
+		ch[ch.length]=new character({
+			'name':'patty',/* the name of the character */
+			'sprite':{'x':0.25,'y':0.25},
+			'pos':{'x':-128,'y':0},
+			'speed':1,/* speed */
+			'speed_max':{'x':1,'y':1},
+			'scale':{'x':1,'y':1},
+			'cell':{
+				'i':0,
+				'p':{'x':0,'y':1},
+				'c':{'x':4,'y':4},
+				's':{'x':128,'y':128}
+			}
+		});
+		ch[ch.length]=new character({
+			'name':'winston',/* the name of the character */
+			'sprite':{'x':1,'y':1},
+			'pos':{'x':-128,'y':100},
+			'speed':0.75,/* speed */
+			'cell':{
+				'i':0,
+				'c':{'x':4,'y':4},
+				'p':{'x':1,'y':1},
+				's':{'x':128,'y':128}
+			}
+		});
 		this.characters=ch;
 	},
 	/* empty the characters array */
@@ -56,20 +73,18 @@ var patty_and_winston={
 	/* key handler */
 	'key':function(e){
 		if(e.code=='ArrowUp'){
-			patty_and_winston.characters[0].dir.y-=0.01;
+			patty_and_winston.characters[patty_and_winston.active_char].dir.y-=0.01;
 		}else if(e.code=='ArrowDown'){
-			patty_and_winston.characters[0].dir.y+=0.01;
+			patty_and_winston.characters[patty_and_winston.active_char].dir.y+=0.01;
 		}else if(e.code=='ArrowLeft'){
-			patty_and_winston.characters[0].dir.x-=0.01;
+			patty_and_winston.characters[patty_and_winston.active_char].dir.x-=0.01;
 		}else if(e.code=='ArrowRight'){
-			patty_and_winston.characters[0].dir.x+=0.01;
+			patty_and_winston.characters[patty_and_winston.active_char].dir.x+=0.01;
 		}
+		console.log(e.code);
 	},
 	/* draw loop */
 	'draw':function(){
-
-		/* clear the viewport */
-		vp.clear();
 
 		/* make the characters array easier to type */
 		var ch=this.characters;
@@ -79,6 +94,27 @@ var patty_and_winston={
 
 		/* if the viewport (vp) exists */
 		if(vp){
+
+			/* clear the viewport */
+			// vp.clear();
+
+			/*this draws the grass this acts the same as vp.clear() */
+			var drawn=false,
+			tile={
+				'image':this.background,
+				'scl':{
+					'x':this.background.width,
+					'y':this.background.height
+				},
+				'pos':{'x':0,'y':0}
+			};
+			while(!drawn){
+				vp.ctx.drawImage(tile.image,tile.pos.x,tile.pos.y,tile.scl.x,tile.scl.y);
+				if(tile.pos.x>vp.size.x){tile.pos.x=0;tile.pos.y+=tile.scl.y;}
+				else tile.pos.x+=tile.scl.x;
+				if(tile.pos.y>vp.size.y&&tile.pos.x>vp.size.x)drawn=true;
+			}
+
 			/* loop through the characters and draw them in the viewport (vp.ctx) */
 			for (var i=0;i<ch.length;i++){
 
@@ -95,14 +131,16 @@ var patty_and_winston={
 				/* calculate the left and right boundary and to reverse the direction force */
 				if(ch[i].dir.x>0){
 					/* the position plus the width of the texture hit the wall */
-					if((ch[i].pos.x+ch[i].tex.width)>vp.size.x){
+					if((ch[i].pos.x+(ch[i].tex.width/ch[i].cell.c.x))>vp.size.x){
 						ch[i].dir.x=0;
+						ch[i].cell.p.y=2;
 						/* this line reverses the direction when the wall is hit */
 						if(this.steady_force.x>0)this.steady_force.x=-0.001;
 					}
 				}else if(ch[i].dir.x<0){
 					if(ch[i].pos.x<0){
 						ch[i].dir.x=0;
+						ch[i].cell.p.y=1;
 						/* this line reverses the direction when the wall is hit */
 						if(this.steady_force.x<0)this.steady_force.x=0.001;
 					}
@@ -110,9 +148,9 @@ var patty_and_winston={
 
 				/* calculate the up and down boundary and stop the direction force */
 				if(ch[i].dir.y>0){
-					if((ch[i].pos.y+ch[i].tex.width)>vp.size.y)
+					if((ch[i].pos.y+ch[i].tex.height/ch[i].cell.c.y)>vp.size.y)
 						ch[i].dir.y=0;
-				}else if(ch[i].dir.y<0){
+				}else if(ch[i].dir.y<ch[i].tex.height/ch[i].cell.c.y){
 					if(ch[i].pos.y<0)
 						ch[i].dir.y=0;
 				}
@@ -132,8 +170,8 @@ var patty_and_winston={
 		html_str+='<h1><img src="img/winston_and_patty_logo.png" alt="Patty and Winston" title="Patty and Winston" /></h1>';
 		html_str+='<canvas id="viewport"></canvas>';
 		html_str+='<div class="buttons">';
-			html_str+='<img src="img/winston_forward.png">';
-			html_str+='<img src="img/patty_forward.png">';
+			html_str+='<button id="winston_select"><img src="img/winston_forward.png"></button>';
+			html_str+='<button id="patty_select"><img src="img/patty_forward.png"></button>';
 		html_str+='</div>';
 		html_str+='<style>';
 		html_str+='body{background-color:#70a52a;}';
@@ -150,8 +188,14 @@ var patty_and_winston={
 	'events':function(create){
 		if(create){
 			event_add(window,'keydown',patty_and_winston.key);
+			event_add(document.getElementById('patty_select'),'click',patty_and_winston.patty_select);
+			event_add(document.getElementById('winston_select'),'click',patty_and_winston.winston_select);
 		}else{
 			event_remove(window,'keydown',patty_and_winston.key);
+			event_remove(document.getElementById('patty_select'),'click',patty_and_winston.patty_select);
+			event_remove(document.getElementById('winston_select'),'click',patty_and_winston.winston_select);
 		}
-	}
+	},
+	'patty_select':function(e){preventEvent(e);patty_and_winston.active_char=0;},
+	'winston_select':function(e){preventEvent(e);patty_and_winston.active_char=1;}
 };
--- src/character.js
+++ src/character.js
@@ -1,25 +1,67 @@
 /* this is the container for characters (stores the characters state) */
-function character(character,x,y,speed,sx,sy,dx,dy,fx,fy) {
+function character(opt) {
+// function character(character,x,y,speed,sx,sy,dx,dy,fx,fy) {
+
+	/* setup the texture */
   	this.tex=new Image();
-  	if(character)
-  		this.tex.src='img/'+character+'_right.png';
-	if(!x)x=0;if(!y)y=0;if(!speed)speed=1;
-	if(!dx)dx=0;if(!dy)dy=0;
-	if(!fx)fx=0;if(!fy)fy=0;
-	if(!sx)sx=1;if(!sy)sy=1;
-  	this.pos={'x':x,'y':y};
-	this.dir={'x':dx,'y':dy};
-	this.fri={'x':fx,'y':fy};
-	this.scl={'x':sx,'y':sy};
+  	if(!opt.tex)this.tex.src='img/spritesheet.png';
+  	else this.tex.src=opt.tex;
+
+  	/* setup the name --> 'name':'Les', <-- */
+	if(!opt.name)opt.name='Anonymous';
+	if(!opt.sprite)opt.sprite={'x':1,'y':1};
+
+	/* setup the position --> 'x':0,'y':0, <-- */
+  	if(!opt.pos)opt.pos={'x':0,'y':0};
+  	this.pos=opt.pos;
+
+  	/* setup the initial direction velocity --> 'dx':0,'dy':0, <-- */
+	if(!opt.dx)opt.dx=0;
+	if(!opt.dy)opt.dy=0;
+	this.dir={'x':opt.dx,'y':opt.dy};
+
+  	/* setup the initial friction dampening --> 'fx':0,'fy':0, <-- */
+	if(!opt.fx)opt.fx=0;
+	if(!opt.fy)opt.fy=0;
+	this.fri={'x':opt.fx,'y':opt.fy};
+
+  	/* setup the initial scale --> 'fx':0,'fy':0, <-- */
+	if(!opt.sx)opt.sx=1;
+	if(!opt.sy)opt.sy=1;
+	this.scl={'x':opt.sx,'y':opt.sy};
+
+  	/* setup the max speed for directions --> 'mxs_x':0,'mxs_y':0, <-- */
+  	if(!opt.mxs)opt.mxs={};
+	if(!opt.mxs.x)opt.mxs.x=0.02;
+	if(!opt.mxs.y)opt.mxs.y=0.02;
 	this.mxs={'x':0.02,'y':0.02};
-  	this.speed=speed;
+
+  	/* setup the max speed for directions --> 'sprite_x':0,'sprite_y':0, <-- */
+  	if(!opt.sprite)opt.sprite={};
+	if(!opt.sprite.x)opt.sprite.x=1;
+	if(!opt.sprite.y)opt.sprite.y=1;
+	this.sprite={'x':opt.sprite.x,'y':opt.sprite.y};
+
+  	/* setup the cell for spritesheet -->  <-- */
+  	if(!opt.cell)opt.cell={
+		'i':0,
+		'c':{'x':4,'y':4},
+		'p':{'x':0,'y':0},
+		's':{'x':64,'y':64}
+
+  	};
+	this.cell=opt.cell;
+
+  	/* setup the speed modifier --> 'sped':0, <-- */
+	if(!opt.speed)opt.speed=1;
+  	this.speed=opt.speed;
 }
 /* draw the character into the viewport (vp.ctx) */
 character.prototype.draw=function(){
 	/* get the texture width and height */
-	var scl={'x':this.tex.width,'y':this.tex.height};
+	var scl={'x':this.tex.width/this.cell.c.x,'y':this.tex.height/this.cell.c.y};
 	/* drop the character into the canvas */
-	vp.ctx.drawImage(this.tex,this.pos.x,this.pos.y,scl.x,scl.y);
+	vp.ctx.drawImage(this.tex,this.cell.p.x*this.cell.s.x,this.cell.p.y*this.cell.s.y,scl.x,scl.y,this.pos.x,this.pos.y,scl.x,scl.y);
 }
 /* set the character direction */
 character.prototype.dir=function(x,y){vp.pos.x=x;vp.pos.y=y;}
