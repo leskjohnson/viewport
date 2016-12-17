14ec9056087343a70c81ca4803b4db1e745affcd
Les K. Johnson <les@leskjohnson.com>
2016-12-17 12:18:47 -0500

    Update the template to be easier to use

--- scn/template.js
+++ scn/template.js
@@ -4,7 +4,24 @@ var template_scene={
 	'start':function(){},
 	'stop':function(){},
 	'draw':function(){},
-	'html':function(){},
-	'sett':function(){},
-	'events':function(){},
+	'html':function(){
+		var html_str='';
+		html_str+='<h1>Template Scene</h1>';
+		html_str+='<p>Content goes here.</p>';
+		return html_str;
+	},
+	'sett':function(){
+		var html_str='';
+		html_str+='<h2>Template Settings</h2>';
+		html_str+='<div id="template_settings_button" class="buttons">';
+			html_str+='<button class="button" onclick="preventEvent(event);">Buttons</button></div>';
+		html_str+='</div>';
+		html_str+='<div class="input">Settings can go here.</div>';
+		return html_str;
+	},
+	'events':function(create){
+		if(create){
+		}else{
+		}
+	},
 };
